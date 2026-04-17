1f739f1be5faea2c9af1f2e511f2a9d4785a960e
Leo-aab <leonardo45abreu@gmail.com>
2026-04-17 12:10:11 -0300

    customizing sway

--- sway.nix
+++ sway.nix
@@ -8,20 +8,35 @@
           xkb_layout "br"
           xkb_variant "abnt2"
       }
-    '';
+     '';
     wrapperFeatures.gtk = true; 
     config = rec {
       modifier = "Mod4";
 
-keybindings = lib.mkOptionDefault {
-  "${modifier}+Return" = "exec ${terminal}";
-  "${modifier}+Shift+q" = "kill";
-  "${modifier}+d" = "exec ${pkgs.fuzzel}/bin/fuzzel";
-  # Printscreen: Selecionar área e copiar para o clipboard
-  "Print" = "exec ${pkgs.grim}/bin/grim -g \"$(${pkgs.slurp}/bin/slurp)\" - | ${pkgs.wl-clipboard}/bin/wl-copy";
+    keybindings = lib.mkOptionDefault {
+     "${modifier}+Return" = "exec ${terminal}";
+      "${modifier}+Shift+q" = "kill";
+       "${modifier}+d" = "exec ${pkgs.fuzzel}/bin/fuzzel";
+        
+       #print
+       "Print" = "exec ${pkgs.grim}/bin/grim -g \"$(${pkgs.slurp}/bin/slurp)\" - | ${pkgs.wl-clipboard}/bin/wl-copy"; 
 };
-
-      # terminal
+      
+ window = {
+    border = 2;
+    hideEdgeBorders = "smart";
+    titlebar = false;
+  };
+   colors = {
+    focused = {
+      border = "#ff7af6";
+      background = "#ff7af6";
+      text = "#ffffff";
+      indicator = "#2e9ef4";
+      childBorder = "#285577";
+    };
+  };    
+       # terminal
       terminal = "kitty"; 
       startup = [
         # Launch Firefox on start
