41516c6bbb7b58501aecb1ed95ed5f52ff92e0c7
Leo-aab <leonardo45abreu@gmail.com>
2026-04-18 13:09:03 -0300

    finally zsh config ended (i think)

--- hm/terminal/zsh.nix
+++ hm/terminal/zsh.nix
@@ -5,6 +5,7 @@
    
    shellAliases = {
    nx = "sudo nixos-rebuild switch --flake";
+   nixop = "sudo nix-store --optimise";
    fx = "sudo nix flake update --flake .";
    cln = "sudo nix-collect-garbage -d";
   
